0f5e584037eee52fd5b9bf5763b232ff60016064
pe4en1ka <t.mamedov1901t@gmail.com>
2025-05-09 22:43:55 +0300

    1.5_ex_5.cpp

---
+++ 1.5_ex_5.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, i = 0;
+    cin >> n;
+    while (n != 0) {
+        cin >> n;
+        i++;
+    }
+    cout << i;
+    return 0;
+}
