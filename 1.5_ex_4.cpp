4c88995a52cc46123e87cd8e41dd51753df368ce
pe4en1ka <t.mamedov1901t@gmail.com>
2025-05-09 22:32:48 +0300

    1.5_ex_4.cpp

---
+++ 1.5_ex_4.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    while (n > 1) {
+        if (n % 2 != 0) {
+            cout << "NO";
+            return 0;
+        }
+        n = n / 2;
+    }
+    cout << "YES";
+    return 0;
+}
