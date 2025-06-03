6549c6cddaa4a7a9b01e43507a0b289272bf6b6d
pe4en1ka <t.mamedov1901t@gmail.com>
2025-06-03 14:58:00 +0300

    1.7_ex_11.cpp

---
+++ 1.7_ex_11.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int n, cnt = 0;
+    cin >> n;
+    vector <int> v(n);
+    for (int i = 0; i < n; i++) {
+        cin >> v[i];
+    }
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (v[i] == v[j]) {
+                cnt++;
+            }
+        }
+    }
+    cout << cnt;
+    return 0;
+}
