fe8d07e28a8f239110ec947f9e216279d1e542ba
pe4en1ka <t.mamedov1901t@gmail.com>
2025-06-04 23:29:45 +0300

    1.8_ex_5.cpp

---
+++ 1.8_ex_5.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int arr[n][n];
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> arr[i][j];
+        }
+    }
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (i < j || i > j) {
+                if (arr[i][j] != arr[j][i]) {
+                    cout << "NO";
+                    return 0;
+                }
+            }
+        }
+    }
+    cout << "YES";
+    return 0;
+}
