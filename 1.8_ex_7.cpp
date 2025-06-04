d68803be21bfc52f97a743919b7e824af183f453
pe4en1ka <t.mamedov1901t@gmail.com>
2025-06-05 00:03:18 +0300

    1.8_ex_8.cpp

---
+++ 1.8_ex_7.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    int transp_arr[m][n];
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cin >> transp_arr[j][i];
+        }
+    }
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            cout << transp_arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
