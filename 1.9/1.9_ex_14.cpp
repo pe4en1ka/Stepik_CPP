a6c180a37fc0d95626fc40f88a9200305f6e0853
pe4en1ka <t.mamedov1901t@gmail.com>
2025-06-11 17:16:02 +0300

    1.9_ex_14.cpp

---
+++ 1.9/1.9_ex_14.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int WaysCounter(int n) {
+    int ways = 0;
+    int arr[n][n];
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+
+        }
+    }
+    return ways;
+}
+int main () {
+    int n;
+    cin >> n;
+    cout << WaysCounter(n);
+    return 0;
+}
