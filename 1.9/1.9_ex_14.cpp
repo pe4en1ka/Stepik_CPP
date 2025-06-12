8cf9af8c524a32e7e7b02d8b8e8b095b2de38897
pe4en1ka <t.mamedov1901t@gmail.com>
2025-06-12 13:24:53 +0300

    1.9_ex_14.cpp

--- 1.9/1.9_ex_14.cpp
+++ 1.9/1.9_ex_14.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
+bool is_under_attack(vector <vector <int> > bd,int x, int y) {
+    return bd[x][y] == -1;
+}
 int WaysCounter(int n) {
     int ways = 0;
-    int arr[n][n];
+    vector <vector <int> > board(n, vector <int>(n, 0));
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
 
