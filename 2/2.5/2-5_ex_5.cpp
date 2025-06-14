25819367388e9d3be73aa8cc2f417930c4ce1de8
pe4en1ka <t.mamedov1901t@gmail.com>
2025-06-15 00:48:45 +0300

    2-5_ex_5.cpp

---
+++ 2/2.5/2-5_ex_5.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    long long l, r;
+    cin >> l >> r;
+    if (l == 1) {
+        cout << 1;
+    }
+    else {
+        cout << 0;
+    }
+}
