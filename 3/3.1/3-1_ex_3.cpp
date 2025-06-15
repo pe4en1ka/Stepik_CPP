1ad283ca160bd2d0a1733c24323b3cf5ede9f03b
pe4en1ka <t.mamedov1901t@gmail.com>
2025-06-15 03:40:53 +0300

    3-1_ex_3.cpp

--- 3/3.1/3-1_ex_3.cpp
+++ 3/3.1/3-1_ex_3.cpp
@@ -5,9 +5,11 @@ int main() {
     int n;
     cin >> n;
     int money[n];
-    int reachest = 0, poorest = ;
     for (int i = 0; i < n; i++) {
         cin >> money[i];
+    }
+    int reachest = money[0], poorest = money[0];
+    for (int i = 1; i < n; i++) {
         if (money[i] > reachest) {
             reachest = money[i];
         }
