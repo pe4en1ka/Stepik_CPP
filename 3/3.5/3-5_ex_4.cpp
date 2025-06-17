c335e8d2d7b25a8d98264365a4a49ee229fbca05
pe4en1ka <t.mamedov1901t@gmail.com>
2025-06-17 18:10:04 +0300

    3-5_ex_4.cpp

---
+++ 3/3.5/3-5_ex_4.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+void strange_sort(int n, int a[]) {
+    if (n == 1) {
+        cout << a[0] << " ";
+        return;
+    }
+    int tmp[n / 2];
+    if (n % 2 == 1) {
+        cout << a[n / 2] << " ";
+        for (int i = n / 2 + 1; i < n; i++) {
+            tmp[i - n / 2 - 1] = a[i];
+        }
+        int m = n;
+        m /= 2;
+        strange_sort(m, tmp);
+        for (int i = 0; i < n / 2; i++) {
+            tmp[i] = a[i];
+        }
+        int q = n;
+        q /= 2;
+        strange_sort(q, tmp);
+    }
+    if (n % 2 == 0) {
+        cout << a[n / 2 - 1] << " ";
+        for (int i = n / 2; i < n; i++) {
+            tmp[i - n / 2] = a[i];
+        }
+        int m = n;
+        m /= 2;
+        strange_sort(m, tmp);
+        int tmp_odd[n / 2 - 1];
+        for (int i = 0; i < n / 2 - 1; i++) {
+            tmp_odd[i] = a[i];
+        }
+        int q = n;
+        q /= 2;
+        strange_sort(q, tmp_odd);
+    }
+}
+
+int main() {
+    int n = 8;
+    int a[] = {1, 2, 3, 4, 5, 6, 7, 8};
+    strange_sort(n, a);
+}
