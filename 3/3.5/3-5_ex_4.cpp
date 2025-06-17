2ac996c44060de64f807c8140cd6ade06a3d16d9
pe4en1ka <t.mamedov1901t@gmail.com>
2025-06-18 01:10:16 +0300

    3-5_ex_4.cpp

--- 3/3.5/3-5_ex_4.cpp
+++ 3/3.5/3-5_ex_4.cpp
@@ -1,49 +1,23 @@
 #include <iostream>
+
 using namespace std;
 
-void strange_sort(int n, int a[]) {
-    if (n == 1) {
-        if (not sizeof(a) == 0) {
-            cout << a[0] << " ";
-        }
+void print_arr(int arr[], int a, int b) {
+    if (a >= b)
         return;
-    }
-    int tmp[n / 2];
-    if (n % 2 == 1) {
-        cout << a[n / 2] << " ";
-        for (int i = n / 2 + 1; i < n; i++) {
-            tmp[i - n / 2 - 1] = a[i];
-        }
-        int m = n;
-        m /= 2;
-        strange_sort(m, tmp);
-        for (int i = 0; i < n / 2; i++) {
-            tmp[i] = a[i];
-        }
-        int q = n;
-        q /= 2;
-        strange_sort(q, tmp);
-    }
-    if (n % 2 == 0) {
-        cout << a[n / 2 - 1] << " ";
-        for (int i = n / 2; i < n; i++) {
-            tmp[i - n / 2] = a[i];
-        }
-        int m = n;
-        m /= 2;
-        strange_sort(m, tmp);
-        int tmp_odd[n / 2 - 1];
-        for (int i = 0; i < n / 2 - 1; i++) {
-            tmp_odd[i] = a[i];
-        }
-        int q = n;
-        q /= 2;
-        strange_sort(q, tmp_odd);
-    }
+
+    cout << arr[(a + b - 1) / 2] << " ";
+    print_arr(arr, (a + b + 1) / 2, b);
+    print_arr(arr, a, (a + b - 1) / 2);
 }
 
-int main() {
-    int n = 8;
-    int a[] = {1, 2, 3, 4, 5, 6, 7, 8};
-    strange_sort(n, a);
+int main(void) {
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    print_arr(arr, 0, n);
+    return 0;
 }
