05d897a132644dfd82e9f6473e2c0d960bac6467
pe4en1ka <t.mamedov1901t@gmail.com>
2025-06-18 02:10:16 +0300

    3-5_ex_5.cpp

--- 3/3.5/3-5_ex_5.cpp
+++ 3/3.5/3-5_ex_5.cpp
@@ -1,13 +1,17 @@
 #include <iostream>
 using namespace std;
 
-int duna(int n) {
+int duna(int n, int cnt) {
+    int c;
+    cnt++;
     if (n < 3) {
         return 0;
     }
     else {
-        return duna(n - 1) + 1;
+        c += duna(n - 1, cnt);
+        c += duna(n - 2);
     }
+    return cnt;
 }
 
 int main() {
