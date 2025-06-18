23cbf89a41abf73792e84c19b53c579b5329211f
pe4en1ka <t.mamedov1901t@gmail.com>
2025-06-18 15:10:15 +0300

    3-5_ex_5.cpp

--- 3/3.5/3-5_ex_5.cpp
+++ 3/3.5/3-5_ex_5.cpp
@@ -2,11 +2,16 @@
 using namespace std;
 
 int duna(int n) {
-
+    if (n == 1 || n == 2) {
+        return 1;
+    }
+    else {
+        return duna(n-1) + duna(n-2);
+    }
 }
 
 int main() {
-    int n, cnt = 0;
+    int n;
     cin >> n;
-    cout << duna(n);
+    cout << duna(n) - 1;
 }
