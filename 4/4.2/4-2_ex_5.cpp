e8be2793131db698dd715fb2a551c297a4656b6b
pe4en1ka <t.mamedov1901t@gmail.com>
2025-06-19 21:10:15 +0300

    4-2_ex_5.cpp

--- 4/4.2/4-2_ex_5.cpp
+++ 4/4.2/4-2_ex_5.cpp
@@ -1,19 +1,20 @@
 #include <iostream>
 using namespace std;
 
-/*void merge(int *b_f, int *e_f, int *b_s, int *e_s, int *b_b) {
-    while (b_f != e_f && b_s != e_s) {
+/* Изначальное решение через вайл, меньше памяти, работа только с указателями
+ *void merge(int *b_f, int *e_f, int *b_s, int *e_s, int *b_b) {
+    while (b_f != e_f + 1 && b_s != e_s + 1) {
         if (*b_f < *b_s) {
             *b_b = *b_f;
             b_f++;
-            if (b_f == e_f) {
+            if (b_f == e_f + 1) {
                 b_b++;
                 break;
             }
         } else {
             *b_b = *b_s;
             b_s++;
-            if (b_s == e_s) {
+            if (b_s == e_s + 1) {
                 b_b++;
                 break;
             }
@@ -21,14 +22,14 @@ using namespace std;
         b_b++;
     }
     // Копируем оставшиеся элементы из первого массива
-    while (b_f != e_f) {
+    while (b_f != e_f + 1) {
         *b_b = *b_f;
         b_f++;
         b_b++;
     }
 
     // Копируем оставшиеся элементы из второго массива
-    while (b_s != e_s) {
+    while (b_s != e_s + 1) {
         *b_b = *b_s;
         b_s++;
         b_b++;
@@ -36,15 +37,16 @@ using namespace std;
 
 }*/
 
-void merge(int *b_f, int *e_f, int *b_s, int *e_s, int *b_b) {
-    long long n = e_f - b_f;
-    long long q = e_s - b_s;
+//Решение через циклы фор, больше затрат памяти работа не только с указателями
+void merge(int *b_f, const int *e_f, int *b_s, const int *e_s, int *b_b) {
+    long long n = e_f - b_f + 1;
+    long long q = e_s - b_s + 1;
     for (int i = 0; i < n + q; i++) {
         if (*b_f <= *b_s ) {
             *b_b = *b_f;
             b_f++;
             b_b++;
-            if (b_f == e_f) {
+            if (b_f == e_f + 1) {
                 break;
             }
         }
@@ -52,13 +54,13 @@ void merge(int *b_f, int *e_f, int *b_s, int *e_s, int *b_b) {
             *b_b = *b_s;
             b_s++;
             b_b++;
-            if (b_s == e_s) {
+            if (b_s == e_s + 1) {
                 break;
             }
         }
     }
-    n = e_f - b_f;
-    q = e_s - b_s;
+    n = e_f - b_f + 1;
+    q = e_s - b_s + 1;
     if (n != 0) {
         for (int i = 0; i < n; i++) {
             *b_b = *b_f;
@@ -69,7 +71,7 @@ void merge(int *b_f, int *e_f, int *b_s, int *e_s, int *b_b) {
     if (q != 0) {
         for (int i = 0; i < q; i++) {
             *b_b = *b_s;
-            b_f++;
+            b_s++;
             b_b++;
         }
     }
@@ -89,7 +91,7 @@ int main() {
         cin >> qq[i];
     }
     int buf[n + q] = {0};
-    merge(nn, nn + n, qq, qq + q, buf);
+    merge(nn, nn + n - 1, qq, qq + q - 1, buf);
     for (auto i : buf) {
         cout << i << " ";
     }
