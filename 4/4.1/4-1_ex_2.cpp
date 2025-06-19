98de4e5ed24151f0dccc574bd2e73ebc50f53542
pe4en1ka <t.mamedov1901t@gmail.com>
2025-06-19 18:10:05 +0300

    4-1_ex_2.cpp

--- 4/4.1/4-1_ex_2.cpp
+++ 4/4.1/4-1_ex_2.cpp
@@ -2,5 +2,18 @@
 using namespace std;
 
 int main() {
-    //put yr code here
+    int n, k;
+    cin >> n >> k;
+    int a[n];
+    int *ptr = a + k;
+    ptr -= k;
+    for (int i = 0; i < n; i++) {
+        *ptr = n - i;
+        ptr++;
+    }
+    for (auto i : a) {
+        cout << i << " ";
+    }
+
+
 }
