975c3cfa89945cbb2e06f517bba71e924d179606
pe4en1ka <t.mamedov1901t@gmail.com>
2025-06-20 00:10:04 +0300

    4-3_ex_1.cpp

--- 4/4.3/4-3_ex_1.cpp
+++ 4/4.3/4-3_ex_1.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 using namespace std;
 
+struct rectangle {
+    int a, b;
+    void set(int s1, int s2) {
+        a = s1;
+        b = s2;
+    }
+    int get() {
+        return a * a + b * b;
+    }
+};
 int main() {
-    //put yr code here
+    int a, b;
+    cin >> a >> b;
+    rectangle r;
+    r.set(a, b);
+    cout << r.get() << endl;
 }
