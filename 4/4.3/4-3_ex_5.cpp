5c20c9c7bf0b416ecefe3066be6e25e350b20513
pe4en1ka <t.mamedov1901t@gmail.com>
2025-06-20 23:10:04 +0300

    4-3_ex_5.cpp

--- 4/4.3/4-3_ex_5.cpp
+++ 4/4.3/4-3_ex_5.cpp
@@ -6,40 +6,66 @@ struct node {
     int val;
 };
 
-int* merge(node *frst, node *second) {
-    while (frst != NULL && second != NULL) {
-        if (frst.val < second.val) {
-            *b_b = *b_f;
-            b_f++;
-            if (b_f == e_f + 1) {
-                b_b++;
+node* merge(node *frst, node *second) {
+    node m[20];
+    for (int i = 0; i < 20; i++) {
+        m[i].val = 0;
+        if (i == 19) {m[i].next = nullptr; continue; }
+        else {m[i].next = m + i + 1;}
+    }
+    node *b_m = m;
+    b_m = m;
+    node *mm = b_m;
+    while (frst != nullptr && second != nullptr) {
+        if ((frst -> val) <= (second -> val)) {
+            (b_m -> val) = (frst -> val);
+            frst = frst -> next;
+            if (frst == nullptr) {
+                b_m = b_m -> next;
                 break;
             }
         } else {
-            *b_b = *b_s;
-            b_s++;
-            if (b_s == e_s + 1) {
-                b_b++;
+            (b_m -> val) = (second -> val);
+            second = second -> next;
+            if (second == nullptr) {
+                b_m = b_m -> next;
                 break;
             }
         }
-        b_b++;
+        b_m = b_m -> next;
     }
     // Копируем оставшиеся элементы из первого массива
-    while (b_f != e_f + 1) {
-        *b_b = *b_f;
-        b_f++;
-        b_b++;
+    while (frst != nullptr) {
+        (b_m -> val) = (frst -> val) ;
+        frst = frst -> next;
+        b_m = b_m -> next;
     }
 
     // Копируем оставшиеся элементы из второго массива
-    while (b_s != e_s + 1) {
-        *b_b = *b_s;
-        b_s++;
-        b_b++;
+    while (second != nullptr) {
+        (b_m -> val) = (second -> val);
+        second = second -> next;
+        b_m = b_m -> next;
     }
+    return mm;
 }
 
 int main() {
-    //put yr code here
+    int n;
+    cin >> n;
+    node frst[n];
+    for (int i = 0; i < n; i++) {
+        cin >> frst[i].val;
+        if (i == n - 1) { frst[i].next = nullptr; continue; }
+        else { frst[i].next = frst + i + 1; }
+    }
+    int q;
+    cin >> q;
+    node second[q];
+    for (int i = 0; i < q; i++) {
+        cin >> second[i].val;
+        if (i == q - 1) { second[i].next = nullptr; continue; }
+        else { second[i].next = second + i + 1; }
+    }
+    cout << merge(frst, second);
 }
