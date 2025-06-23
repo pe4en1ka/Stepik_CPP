30908fb5209740409a0b7d18fe2592f367bfff8c
pe4en1ka <t.mamedov1901t@gmail.com>
2025-06-23 23:10:05 +0300

    4-6_ex_3.cpp

--- 4/4.6/4-6_ex_3.cpp
+++ 4/4.6/4-6_ex_3.cpp
@@ -25,8 +25,44 @@ int main() {
         else if (t == 4) {
             int j;
             cin >> j;
-            if (j < 0 || j >= n) {
-                cout << ""
+            if (j < 0 || j >= v.size()) {
+                cout << "Out of bounds" << endl;
+            }
+            else {
+                cout << v[j] << endl;
+            }
+        }
+        else if (t == 5) {
+            cout << v.size() << endl;
+        }
+        else if (t == 6) {
+            int val;
+            cin >> val;
+            v.push_back(val);
+        }
+        else if (t == 7) {
+            if (not v.empty()) {
+                v.pop_back();
+            }
+        }
+        else if (t == 8) {
+            int j;
+            cin >> j;
+            if (not (j < 0 || j >= v.size())) {
+                v.erase(v.begin() + j);
+            }
+        }
+        else if (t == 9) {
+            int j, val;
+            cin >> j >> val;
+            v.insert(v.begin() + j + 1, val);
+        }
+        else if (t == 10) {
+            if (not (v.empty())) {
+                cout << v[v.size() - 1] << endl;
+            }
+            else {
+                cout << "empty" << endl;
             }
         }
     }
