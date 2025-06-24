85327e67a47bd4e7419768f16f522a90ebb818a7
pe4en1ka <t.mamedov1901t@gmail.com>
2025-06-24 14:10:07 +0300

    4-8_ex_7.cpp

---
+++ 4/4.8/4-8_ex_7.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int mod;
+
+struct Int {
+    int val;
+    Int()
+        : val(0) {}
+    Int(int a)
+        : val(a) {}
+
+};
+
+int main() {
+    //put yr code here
+}
