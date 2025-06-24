9e5ea39e03319d74e7ff7af5d7d32b1c5b338cc4
pe4en1ka <t.mamedov1901t@gmail.com>
2025-06-24 03:10:16 +0300

    test.cpp

---
+++ 4/4.7/test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+    std::string input;
+    while (std::getline(std::cin, input)) {
+        std::cout << "Received: " << input << std::endl;
+    }
+    if (std::cin.eof()) {
+        std::cout << "EOF detected (Ctrl+D pressed)";
+    }
+    return 0;
+}
