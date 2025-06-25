d321beeafa88e433f6061d507887a0a5b476a213
pe4en1ka <t.mamedov1901t@gmail.com>
2025-06-25 23:10:11 +0300

    5-3_ex_6.cpp

--- 5/5.2/5-3_ex_6.cpp
+++ 5/5.2/5-3_ex_6.cpp
@@ -9,17 +9,30 @@ int main() {
 
     string n;
     cin >> n;
-
+    int i = static_cast<int> (n.size());
+    int j = i - 1;
+    vector <bool> two;
     while (!n.empty()) {
-        char dig = n[n.size() - 1];
+        char dig = n[i - 1];
         int dec;
-        vector<int> tmp;
+        vector<bool> tmp(3, false);
         if (dig <= '9') {
             dec = int(dig) - 48;
         }
         else {
             dec = 10 + int(dig) - 65;
         }
-
+        n.pop_back();
+        int k = 2;
+        while (dec >= 2) {
+            tmp[k] = dec % 2;
+            dec /= 2;
+            k--;
+        }
+        two[j] = dec; two[j - 1] = tmp[2]; two[j - 2] = tmp[1]; two[j - 3] = tmp[0];
+        j -= 4;
+    }
+    for (auto i : two) {
+        cout << i;
     }
 }
