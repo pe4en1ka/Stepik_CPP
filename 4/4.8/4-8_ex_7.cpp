fd4e740d3f3bb7f9a6d90ab86949a5771f618b45
pe4en1ka <t.mamedov1901t@gmail.com>
2025-06-25 15:10:04 +0300

    4-8_ex_7.cpp

--- 4/4.8/4-8_ex_7.cpp
+++ 4/4.8/4-8_ex_7.cpp
@@ -1,20 +1,32 @@
 #include <iostream>
 using namespace std;
 
-int mod;
+long long mod;
 
 struct Int {
-    int val;
+    long long val;
     Int()
         : val(0) {}
     Int(int a)
-        : val(a) {}
-    Int operator + (Int a, Int b) {
-        int sum = (int) a + (int) b;
-
-    }
+        : val((a % mod + mod) % mod) {}
 };
+Int operator + (Int a, Int b)  {
+        long long sum =  a.val + b.val;
+        Int s = (sum % mod + mod) % mod;
+        return s;
+    }
 
+    Int operator - (Int a, Int b)  {
+        long long diff = a.val - b.val;
+        Int d = (diff % mod + mod) % mod;
+        return d;
+    }
+
+    Int operator * (Int a, Int b)  {
+        long long mult = a.val * b.val;
+        Int m = (mult % mod + mod) % mod;
+        return m;
+    }
 int main() {
     //put yr code here
 }
