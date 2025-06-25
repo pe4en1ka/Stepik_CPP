8fa7e58482dbfb1f7c37a64f5d6fa5c05815e273
pe4en1ka <t.mamedov1901t@gmail.com>
2025-06-25 21:10:05 +0300

    5-2_ex_3.cpp

---
+++ 5/5.2/5-2_ex_3.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    long long n;
+    short int k;
+    cin >> n >> k;
+    vector <char> ka;
+    while (n >= k) {
+        int tmp = n % k;
+        char dig;
+        if ( 0 <= tmp && tmp <= 9) {
+            
+        }
+    }
+}
