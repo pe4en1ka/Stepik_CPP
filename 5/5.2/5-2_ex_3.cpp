b2f7750b5a7fc328146297a9f2766e6c2acbddf9
pe4en1ka <t.mamedov1901t@gmail.com>
2025-06-25 22:10:04 +0300

    5-2_ex_3.cpp

--- 5/5.2/5-2_ex_3.cpp
+++ 5/5.2/5-2_ex_3.cpp
@@ -15,7 +15,21 @@ int main() {
         int tmp = n % k;
         char dig;
         if ( 0 <= tmp && tmp <= 9) {
-            
+            dig = char(48 + tmp);
         }
+        else if (10 <= tmp * tmp <= 36) {
+            dig = char(65 + tmp - 10);
+        }
+        ka.push_back(dig);
+        n /= k;
+    }
+    if ( 0 <= n && n <= 9) {
+        cout << char(48 + n);
+    }
+    else if (10 <= n && n <= 36) {
+        cout << char(65 + n - 10);
+    }
+    for (int i = static_cast<int> (ka.size()) - 1; i >= 0; i--) {
+        cout << ka[i];
     }
 }
