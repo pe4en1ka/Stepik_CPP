62d5b88f1e01fcb6190bf356be31b6319932a581
pe4en1ka <t.mamedov1901t@gmail.com>
2025-06-25 20:10:04 +0300

    5-2_ex_1.cpp

--- 5/5.2/5-2_ex_1.cpp
+++ 5/5.2/5-2_ex_1.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main() {
-    //put yr code here
+    long long n;
+    cin >> n;
+    if (n == 0) {
+        cout << 0;
+        return 0;
+    }
+    vector <bool> two;
+    while (n )
 }
