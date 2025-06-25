754bb32fe15b733af97982253ffabbbdd297e2d8
pe4en1ka <t.mamedov1901t@gmail.com>
2025-06-25 16:10:04 +0300

    5-1_ex_1.cpp

--- 5/5.1/5-1_ex_1.cpp
+++ 5/5.1/5-1_ex_1.cpp
@@ -2,5 +2,17 @@
 using namespace std;
 
 int main() {
-    //put yr code here
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int n;
+    cin >> n;;
+    long long sum = 0;
+    for (int i = 0; i < n; i++) {
+        int tmp;
+        cin >> tmp;
+        sum += tmp;
+    }
+    cout << sum;
 }
