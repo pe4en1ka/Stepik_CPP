1d7700322a57909229600a291f23cb4fcae1dfc3
pe4en1ka <t.mamedov1901t@gmail.com>
2025-06-25 21:10:04 +0300

    5-2_ex_1.cpp

--- 5/5.2/5-2_ex_1.cpp
+++ 5/5.2/5-2_ex_1.cpp
@@ -10,5 +10,12 @@ int main() {
         return 0;
     }
     vector <bool> two;
-    while (n )
+    while (n >= 2) {
+        two.push_back(n % 2);
+        n /= 2;
+    }
+    cout << n;
+    for (int i = static_cast<int> (two.size()) - 1; i >= 0; i--) {
+        cout << two[i];
+    }
 }
