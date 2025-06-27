79a4c70b143138a72df6c81ea9a3edde7b555378
pe4en1ka <t.mamedov1901t@gmail.com>
2025-06-27 22:10:04 +0300

    5-5_ex_2.cpp

---
+++ 5/5.5/5-5_ex_2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int n;
+    cin >> n;
+    long long res;
+    cin >> res;
+    for (int i = 1; i < n; i ++) {
+        long long tmp;
+        cin >> tmp;
+        res ^= tmp;
+    }
+    cout << res;
+}
