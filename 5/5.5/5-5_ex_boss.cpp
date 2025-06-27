2e16eaaecc7c0317632c7a1fe9b079db5675e9f4
pe4en1ka <t.mamedov1901t@gmail.com>
2025-06-28 00:10:04 +0300

    5-5_ex_boss.cpp

--- 5/5.5/5-5_ex_boss.cpp
+++ 5/5.5/5-5_ex_boss.cpp
@@ -1,46 +1,29 @@
-#include <iostream>
+#include <bits/stdc++.h>
 #include <vector>
 using namespace std;
-
-vector <unsigned long long> dec_to_t(unsigned long long dec, int t) {
-    vector <unsigned long long> res;
-    while (dec >= t) {
-        res.push_back(dec % t);
-        dec /= t;
-    }
-    res.push_back(dec);
-    return res;
-}
-
-unsigned long long power(unsigned long long a, int b) {
-    unsigned long long res = 1;
-    for (int i = 0; i < b; i++) {
-        res *= a;
-    }
-    return res;
-}
-
-unsigned long long k_to_dec(int k, vector <int> v) {
-    unsigned long long res = 0;
-    for (int i = 0; i < v.size(); i++) {
-        res += power(k, static_cast<int>(v.size()) - i - 1) * v[i];
-    }
-    return res;
-}
-
-
+#define ull unsigned long long
 
 int main() {
-    int k, t;
+    ull a, k, t, ost;
     cin >> k >> t;
-    vector <int> k_num;
-    int dig;
-    while (cin >> dig) {
-        k_num.push_back(dig);
+    vector <ull> v;
+    deque<ull> ans;
+    while (cin >> a) {
+        v.push_back(a);
     }
-    unsigned long long dec = k_to_dec(k, k_num);
-    vector <unsigned long long> t_num = dec_to_t(dec, t);
-    for (int i = static_cast<int>(t_num.size()) - 1; i >= 0 ; i--) {
-        cout << t_num[i] << " ";
+    bool ok = true;
+    while (ok) {
+        ost = 0;
+        ok = false;
+        for (auto &i : v) {
+            ost = (ost *k + i);
+            i = ost / t;
+            ost = ost % t;
+            ok |= i;
+        }
+        ans.push_front(ost);
+    }
+    for (auto i : ans) {
+        cout << i << " ";
     }
 }
