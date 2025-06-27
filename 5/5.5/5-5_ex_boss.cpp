7ae02393bf64fa901927302f990b50a7084327b2
pe4en1ka <t.mamedov1901t@gmail.com>
2025-06-27 22:10:05 +0300

    5-5_ex_boss.cpp

---
+++ 5/5.5/5-5_ex_boss.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+
+int main() {
+    int k, t;
+    cin >> k >> t;
+    vector <int> k_num;
+    int dig;
+    while (cin >> dig) {
+        k_num.push_back(dig);
+    }
+
+}
