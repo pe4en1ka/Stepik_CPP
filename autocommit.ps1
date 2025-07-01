b9efd5c67c3f82206e3df818cce4c1b797829c89
Auto Committer <auto@commit>
2025-07-02 00:31:18 +0300

    Автоматический коммит

--- autocommit.ps1
+++ autocommit.ps1
@@ -5,7 +5,7 @@ $RepoPath = "C:\Users\tmame\MIPT_1"
 $GitPath = "C:\Program Files\Git\bin\git.exe"
 $GitHubToken = "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"  # ЗАМЕНИТЕ НА РЕАЛЬНЫЙ ТОКЕН
 $GitHubUser = "tmame"  # Ваш GitHub username
-$RepoName = "MIPT#1"   # Название репозитория
+$RepoName = "MIPT_1"   # Название репозитория
 
 function Log {
     param([string]$Message)
