7781fad00e5f2e855f755a46bcace62aaf5876ac
Auto Committer <auto@commit>
2025-07-14 00:10:03 +0300

    Автоматический коммит 2025-07-14 00:10:03

--- 2/2.4/2-4_ex_2.cpp
+++ 2/2.4/2-4_ex_2.cpp
@@ -9,13 +9,18 @@ ll Mfastpow(ll a, ll n) {
         return 1;
     }
     if (n % 2 == 1) {
-        return (a * Mfastpow(a, n - 1)) % m;
+        return ((a * Mfastpow(a, n - 1)) % m + m) % m;
     }
     ll tmp = Mfastpow(a, n / 2);
-    return (tmp * tmp) % m;
+    return ((tmp * tmp) % m + m) % m;
 }
 
-ll Mmult
+ll Mmult(ll a, ll b) {
+    return ((a % m * b % m) % m + m) % m;}
+
+ll Mdiv(ll a, ll b) {
+    return Mmult(a, Mfastpow(b, m - 2));
+}
 
 int main() {
     ios::sync_with_stdio(false);
@@ -24,5 +29,7 @@ int main() {
 
     int a, b, c, d;
     cin >> a >> b >> c >> d;
-
+    ll ab = Mdiv(a, b);
+    ll cd = Mdiv(c, d);
+    cout << ((ab + cd) % m + m) % m;
 }
