b1e01c1978023e2ea7349f38cf9bfd11da831bc1
Auto Committer <auto@commit>
2025-07-16 22:10:03 +0300

    Автоматический коммит 2025-07-16 22:10:03

--- 2/2.4/2-4_ex_4.cpp
+++ 2/2.4/2-4_ex_4.cpp
@@ -5,12 +5,30 @@ using namespace std;
 constexpr ll mod = 1e9 + 7;
 
 ll f(ll n) {
-    if (n == 1 || n == 0) {
+    ll res = 1;
+    for (ll i = 2; i <= n; i++) {
+        res = (res * i) % mod;
+    }
+    return (res);
+}
+
+ll Mfastpow(ll a, ll n) {
+    if (n == 0) {
         return 1;
     }
-    else {
-        return n * f(n - 1);
+    if (n % 2 == 1) {
+        return ((a * Mfastpow(a, n - 1)) % mod + mod) % mod;
     }
+    ll tmp = Mfastpow(a, n / 2);
+    return ((tmp * tmp) % mod + mod) % mod;
+}
+
+ll Mmult(ll a, ll b) {
+    return ((a * b) % mod + mod) % mod;
+}
+
+ll Mdiv (ll a, ll b) {
+    return Mmult(a,Mfastpow(b, mod - 2));
 }
 
 int main() {
@@ -26,5 +44,15 @@ int main() {
     }
     else {
         ll sum = 0;
+        ll fac_n = f(n);
+        for (ll k = 1; k <= l && m * k <= n; k++) {
+            ll fac_mk = f(m * k);
+            ll fac_nmk = f(n - m * k);
+            sum = (sum + Mdiv(fac_n, Mmult(fac_mk, fac_nmk))) % mod;
+
+
+        }
+        cout << sum % mod;
+        return 0;
     }
 }
