034ae18b4fc7b2d3dae32a6d7d9df76e87520507
Auto Committer <auto@commit>
2025-07-16 20:10:03 +0300

    Автоматический коммит 2025-07-16 20:10:03

--- 2/2.4/2-4_ex_3.cpp
+++ 2/2.4/2-4_ex_3.cpp
@@ -1,11 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define ll long long
 
-
+ll extgsd(ll a, ll b, ll & x, ll & y ) {
+    if ( a == 0) {
+        x = 0;
+        y = 1;
+        return b;
+    }
+    else {
+        ll x1, y1;
+        ll d = extgsd( b % a, a, x1, y1 );
+        x = y1 - ( b / a ) * x1;
+        y = x1;
+        return d;
+    }
+}
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
-    
+
+    ll a, n ,m;
+    cin >> a >> n >> m;
+    ll x, y, rev;
+    ll gsd = extgsd(a, m, x, y);
+    if (gsd != 1) {
+        cout << -1;
+        return 0;
+    }
+    else {
+        rev = (x % m + m) % m;
+    }
 }
