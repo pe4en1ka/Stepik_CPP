48b0f2ea16737579ef3af6e9f533f4e9dbb090ed
Auto Committer <auto@commit>
2025-07-20 19:10:04 +0300

    Автоматический коммит 2025-07-20 19:10:04

--- 3/3.1/3-2_ex_3.cpp
+++ 3/3.1/3-2_ex_3.cpp
@@ -1,9 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+constexpr int MAXN = 1000500;
+int a[MAXN];
+int n;
+
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
-    
+
+    cin >> n;
+    a[1] = 0;
+    for (int i = 2; i <= n; i++) {
+        a[i] = a[i - 1] + 1;
+        if (i % 2 == 0) {
+            a[i] = min(a[i], a[i / 2] + 1);
+        }
+        if (i % 3 == 0) {
+            a[i] = min(a[i], a[i / 3] + 1);
+        }
+    }
+    cout << a[n];
 }
