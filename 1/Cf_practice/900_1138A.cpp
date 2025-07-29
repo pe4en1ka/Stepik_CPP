4b4637afc5ab028d41ac367510799de13d490bac
Auto Committer <auto@commit>
2025-07-29 19:10:03 +0300

    Автоматический коммит 2025-07-29 19:10:03

--- 1/Cf_practice/900_1138A.cpp
+++ 1/Cf_practice/900_1138A.cpp
@@ -12,31 +12,45 @@ int main() {
     for (int i = 0; i < n; i++) {
         char tmp;
         cin >> tmp;
-        if (tmp == 2) {
+        if (tmp == 50) {
             a[i] = true;
         }
     }
-    int max = 0, cnt1 = 0, cnt2 = 0;
+    int maxx = 0, cnt1 = 0, cnt2 = 0; auto l1 = distance(a.begin(),find(a.begin(), a.end(), false)), l2 = distance(a.begin(), find(a.begin(), a.end(), true));
     bool cur = a[0];
     for (int i = 1; i < n; i++) {
         if (a[i] == cur) {
             if (cur == false) {
+                l1 = i;
                 cnt1++;
             }
             else {
+                l2 = i;
                 cnt2++;
             }
             continue;
         }
         else {
-            if ()
+            if (cnt1 >= cnt2 || cnt1 <= cnt2) {
+                maxx = max(2 * min(cnt1, cnt2),maxx);
+            }
             cur = a[i];
             if (cur == false) {
+                if (l1 != i) {
+                    cnt1 = 0;
+                }
                 cnt1++;
             }
             else {
+                if (l2 != i) {
+                    cnt2 = 0;
+                }
                 cnt2++;
             }
         }
     }
+    if (cnt1 >= cnt2 || cnt1 <= cnt2) {
+        maxx = max(2 * min(cnt1, cnt2),maxx);
+    }
+    cout << maxx << endl;
 }
