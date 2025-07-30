dbcfd5f9223f4abc4da12914ffd6e39d7d376f28
Auto Committer <auto@commit>
2025-07-30 22:10:03 +0300

    Автоматический коммит 2025-07-30 22:10:03

--- 3/3.3/3-3_ex_5.cpp
+++ 3/3.3/3-3_ex_5.cpp
@@ -21,21 +21,8 @@ int main() {
             a[i][0] = 1;
         }
     }
-    for (int i = 1; i < n; i++) {
-        for (int j = 1; j < m; j++) {
-            if (i >= 2) {
-                a[i][j] =(a[i][j] % MOD + a[i - 2][j - 1] % MOD) % MOD;
-                if (j + 1 <= m) {
-                    a[i][j] =(a[i][j] % MOD + a[i - 2][j + 1] % MOD) % MOD;
-                }
-            }
-            if (j >= 2) {
-                a[i][j] = (a[i][j] % MOD + a[i - 1][j - 2] % MOD) % MOD;
-                if (i + 1 <= n) {
-                    a[i][j] = (a[i][j] % MOD + a[i + 1][j - 2] % MOD) % MOD;
-                }
-            }
-        }
+    for (int k = 0; k < m + n - 1; k++) {
+        int s = max( )
     }
     cout << a[n - 1][m - 1] << endl;
 }
