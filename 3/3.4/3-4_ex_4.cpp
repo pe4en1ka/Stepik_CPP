24b9a6e2a9cb06ded65dd0c1cd6094d77653eb22
Auto Committer <auto@commit>
2025-08-07 23:10:04 +0300

    Автоматический коммит 2025-08-07 23:10:04

--- 3/3.4/3-4_ex_4.cpp
+++ 3/3.4/3-4_ex_4.cpp
@@ -16,34 +16,62 @@ int main() {
         }
     }
     vector <vector <pair<pair<int, int>, ll>>> dp(n, vector <pair<pair<int, int>, ll>>(m, {{0,0}, LLONG_MAX}));
-    dp[2][2] = {{1,1}, a[0][0]};
+    dp[0][0].second = a[0][0];
+    dp[0][0].first = {-1, -1};
     for ( int k = 0; k < n + m + 1; k++) {
         int s = max(0, k - m + 1);
         int e = min(k, n - 1);
         for (int i = s; i <= e; i++) {
             int j = k - i;
-            dp[i][j].second = min({dp[i - 2][j + 1].second, dp[i - 2][j - 1]. second, dp[i - 1][j - 2].second, dp[i + 1][j - 2].second}) + a[i][j];
-
+            ll minn = LLONG_MAX;
+            pair<int,int> best_move = {-1, -1};
+            if (i >= 2 && j + 1 < m) {
+                if (dp[i - 2][j + 1].second < minn) {
+                    minn = dp[i - 2][j + 1].second;
+                    best_move = {i - 2, j + 1};
+                }
+            }
+            if (i >= 2 && j - 1 >= 0) {
+                if (dp[i - 2][j - 1].second < minn) {
+                    minn = dp[i - 2][j - 1].second;
+                    best_move = {i - 2, j - 1};
+                }
+            }
+            if (i - 1 >= 0 && j - 2 >= 0) {
+                if (dp[i - 1][j - 2].second < minn) {
+                    minn = dp[i - 1][j - 2].second;
+                    best_move = {i - 1, j - 2};
+                }
+            }
+            if (i + 1 < n && j - 2 >= 0) {
+                if (dp[i + 1][j - 2].second < minn) {
+                    minn = dp[i + 1][j - 2].second;
+                    best_move = {i + 1, j - 2};
+                }
+            }
+            if (minn != LLONG_MAX) {
+                dp[i][j].second = minn + a[i][j];
+                dp[i][j].first = best_move;
+            }
         }
     }
-    if (dp[n + 1][m + 1].second > 1000000000) {
+    if (dp[n - 1][m - 1].second == LLONG_MAX) {
         cout << "NO" << '\n';
-        return 0;
     }
     else {
         cout << "YES" << '\n';
         vector <pair<int, int>> r;
-        int x = n + 1;
-        int y = m + 1;
-        while (x != 1 && y != 1) {
+        int x = n - 1;
+        int y = m - 1;
+        while (x != -1 || y != -1) {
             r.emplace_back(x, y);
             int old_x = x, old_y = y;
             x = dp[old_x][old_y].first.first;
             y = dp[old_x][old_y].first.second;
         }
-        cout << dp[n + 1][y + 1].second << " " << r.size() - 1 << '\n';
+        cout << dp[n - 1][m - 1].second << " " << r.size() << '\n';
         for (int i = static_cast<int>(r.size() - 1); i >= 0; i--) {
-            cout << r[i].first << " " << r[i].second << '\n';
+            cout << r[i].first + 1 << " " << r[i].second + 1 << '\n';
         }
     }
 
