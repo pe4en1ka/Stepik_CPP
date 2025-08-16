8ad6db20b0eb67558984477e7473b8c774e3d22e
Auto Committer <auto@commit>
2025-08-16 15:10:03 +0300

    Автоматический коммит 2025-08-16 15:10:03

--- 3/3.4/3-4_ex_5.cpp
+++ 3/3.4/3-4_ex_5.cpp
@@ -11,37 +11,46 @@ int main() {
     cin >> n >> m;
     vector <vector<int>> a(n, vector<int>(m));
     vector <vector<ll>> dp(n, vector<ll>(m));
+    vector <ll> le(m - 1);
+    vector <ll> up(n - 1);
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
             cin >> a[i][j];
         }
     }
     dp[0][0] = a[0][0];
-    ll min_le = dp[0][0];
-    ll min_up = dp[0][0];
+    pair <ll, int> min_le = {dp[0][0], 0};
+    pair <ll, int> min_up = min_le;
     for (int i = 1; i < n; i++) {
-        dp[i][0] = min_up + a[i][0];
-        if (min_up > dp[i][0]) {
-            min_up = dp[i][0];
+        dp[i][0] = min_up.first + a[i][0];
+        if (min_up.first > dp[i][0]) {
+            min_up.first = dp[i][0];
         }
     }
     for (int j = 1; j < m; j++) {
-        dp[0][j] = min_le + a[0][j];
-        if (min_le > dp[0][j]) {
-            min_le = dp[0][j];
+        dp[0][j] = min_le.first + a[0][j];
+        if (min_le.first > dp[0][j]) {
+            min_le.first = dp[0][j];
         }
     }
-    min_le = dp[1][0];
-    min_up = dp[0][1];
+    for (int i = 0; i < n - 1; i++) {
+        le[i] = dp[i + 1][0];
+    }
+    for (int j = 0; j < m - 1; j++) {
+        up[j] = dp[0][j + 1];
+    }
+    min_le = {le[0], 1};
+    min_up = {up[0], 1};
     for (int i = 1; i < n; i++) {
         for (int j = 1; j < m; j++) {
-            if (min_le > dp[i][j - 1]) {
-                min_le = dp[i][j - 1];
+            dp[i][j] = min(min_le.first,min_up.first) + a[i][j];
+            if (min_le.first > dp[i][j]) {
+                min_le.first = dp[i][j];
+                min_le.second = j;
             }
-            if (min_up > dp[i - 1][j]) {
-                min_up = dp[i - 1][j];
+            if (min_up.first > dp[i][j]) {
+                min_up.first = dp[i][j];
             }
-            dp[i][j] = min(min_le,min_up) + a[i][j];
         }
     }
     for (const auto& i : dp) {
