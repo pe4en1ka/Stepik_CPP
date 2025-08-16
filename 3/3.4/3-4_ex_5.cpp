3aa15281662fc4f3094434b5a7e305001d86ab2a
Auto Committer <auto@commit>
2025-08-16 17:10:03 +0300

    Автоматический коммит 2025-08-16 17:10:03

--- 3/3.4/3-4_ex_5.cpp
+++ 3/3.4/3-4_ex_5.cpp
@@ -11,59 +11,69 @@ int main() {
     cin >> n >> m;
     vector <vector<int>> a(n, vector<int>(m));
     vector <vector<pair<ll, pair<int, int>>>> dp(n, vector<pair<ll, pair<int, int>>>(m));
-    vector <ll> le(n - 1);
-    vector <ll> up(m - 1);
+    vector <pair<ll, int>> le(n - 1);
+    vector <pair<ll, int>> up(m - 1);
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
             cin >> a[i][j];
         }
     }
     dp[0][0] = {a[0][0], {-1, -1}};
-    pair <ll, int> min_le = {dp[0][0].first, 0};
-    pair <ll, int> min_up = min_le;
+    ll min_le = dp[0][0].first;
+    ll min_up = min_le;
     pair <int, int> best_move = {0, 0};
     for (int i = 1; i < n; i++) {
-        dp[i][0].first = min_up.first + a[i][0];
+        dp[i][0].first = min_up + a[i][0];
         dp[i][0].second = best_move;
-        if (min_up.first > dp[i][0].first) {
-            min_up.first = dp[i][0].first;
+        if (min_up > dp[i][0].first) {
+            min_up = dp[i][0].first;
             best_move = {i, 0};
         }
     }
     best_move = {0, 0};
     for (int j = 1; j < m; j++) {
-        dp[0][j].first = min_le.first + a[0][j];
+        dp[0][j].first = min_le + a[0][j];
         dp[0][j].second = best_move;
-        if (min_le.first > dp[0][j].first) {
-            min_le.first = dp[0][j].first;
+        if (min_le > dp[0][j].first) {
+            min_le = dp[0][j].first;
             best_move = {0, j};
         }
     }
     for (int i = 0; i < n - 1; i++) {
-        le[i] = dp[i + 1][0].first;
+        le[i].first = dp[i + 1][0].first;
+        le[i].second = 0;
     }
     for (int j = 0; j < m - 1; j++) {
-        up[j] = dp[0][j + 1].first;
+        up[j].first = dp[0][j + 1].first;
+        up[j].second = 0;
     }
-
     for (int i = 1; i < n; i++) {
         for (int j = 1; j < m; j++) {
-            min_le = {le[i - 1], i};
-            min_up = {up[j - 1], j};
-            best
-            dp[i][j].first = min(min_le.first,min_up.first) + a[i][j];
-            if (min_le.first > dp[i][j].first) {
-                le[i - 1] = dp[i][j].first;
+            min_le = le[i - 1].first;
+            min_up = up[j - 1].first;
+            if (min_le > min_up) {
+                best_move = {up[j - 1].second, j};
             }
-            if (min_up.first > dp[i][j].first) {
-                up[j - 1] = dp[i][j].first;
+            else {
+                best_move = {i, le[i - 1].second};
+            }
+            dp[i][j].first = min(min_le,min_up) + a[i][j];
+            dp[i][j].second = best_move;
+            if (min_le > dp[i][j].first) {
+                le[i - 1].first = dp[i][j].first;
+                le[i - 1].second = j;
+            }
+            if (min_up > dp[i][j].first) {
+                up[j - 1].first = dp[i][j].first;
+                up[j - 1].second = i;
             }
         }
     }
     for (const auto& i : dp) {
         for (auto j : i) {
-            cout << j << " ";
+            cout << j.first << ", " << j.second.first + 1 << " " << j.second.second + 1<< "\t";
         }
         cout << endl;
     }
+     
 }
