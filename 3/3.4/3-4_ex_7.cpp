14779d8f1a4ef613e1edc3a6751c051d949376c9
Auto Committer <auto@commit>
2025-08-26 18:10:03 +0300

    Автоматический коммит 2025-08-26 18:10:03

--- 3/3.4/3-4_ex_7.cpp
+++ 3/3.4/3-4_ex_7.cpp
@@ -10,5 +10,22 @@ int main() {
 
     int n, k;
     cin >> n >> k;
-    vector <ll>
+    vector <ll> a(n + k);
+    for (int i = k; i < n + k; i++) cin >> a[i];
+    vector <pair <ll, int>> dp(n, {INF,-1});
+    pair <ll,int>  min_win = {0, -1};
+    for (int i = 0; i < k; i++) {
+        dp[i].first = min_win.first + a[i + k];
+        dp[i].second = min_win.second;
+
+        if (dp[i].first < min_win.first) {
+            min_win = {dp[i].first, i};
+        }
+    }
+    min_win = {dp[0].first, 0};
+    for (int i = 1; i < k; i++) {
+        if (dp[i].first < min_win.first) {
+            min_win = {dp[i].first, i};
+        }
+    }
 }
