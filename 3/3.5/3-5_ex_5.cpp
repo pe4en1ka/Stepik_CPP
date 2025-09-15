324e8adf07e67c5b86f3a048514db10341de38f6
Auto Committer <auto@commit>
2025-09-15 18:10:03 +0300

    Автоматический коммит 2025-09-15 18:10:03

--- 3/3.5/3-5_ex_5.cpp
+++ 3/3.5/3-5_ex_5.cpp
@@ -1,6 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+bool comppair(const pair<int, int> &a, const pair<int, int> &b) {
+    return a.first > b.first;
+}
+
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
@@ -17,5 +21,25 @@ int main() {
         cin >> c[i];
     }
 
-    
+    for (int i = 0; i < n; i++) {
+        
+    }
+
+    vector <pair <long double, int>> ben(n);
+    for (int i = 0; i < n; i++) {
+        ben[i].first = static_cast<double>(c[i] / w[i]);
+        ben[i].second = i;
+    }
+    sort(ben.begin(), ben.end(), comppair);
+    int cnt = 0, wei = 0;
+    for (int i = 0; i < n; i++) {
+        int cur_w = w[ben[i].second];
+        int cur_c = c[ben[i].second];
+        if (s >= wei + cur_w ) {
+            cnt += cur_c;
+        }
+        else {
+
+        }
+    }
 }
