d805cbe4dee67ebb9bb62e433bb7879de715ff8c
Auto Committer <auto@commit>
2025-09-20 19:10:03 +0300

    Автоматический коммит 2025-09-20 19:10:03

--- 4/4.2/4-2_ex_2.cpp
+++ 4/4.2/4-2_ex_2.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void dfs(int v, vector <bool> &us, map <int, set<int>> &lg, int &cnt, set <int> path) {
+void dfs(int v, vector <bool> &us, map <int, set<int>> &lg, int &cnt, set <int> &path) {
     us[v - 1] = true;
     path.insert(v);
     cnt++;
@@ -10,7 +10,6 @@ void dfs(int v, vector <bool> &us, map <int, set<int>> &lg, int &cnt, set <int> 
             continue;
         }
         dfs(u, us, lg, cnt, path);
-        path.insert(v);
     }
 }
 
@@ -31,10 +30,21 @@ int main() {
     set <int> path;
     int peaks = 0; int st = 0; int con_comp = 0;
     while (peaks != n) {
-        for (auto i : used) if (!used[i]) st = i; break;
+        for (int i = 0; i < n; i++) {
+            if (!used[i]) {
+                st = i + 1;
+                break;
+            }
+        }
         dfs(st, used, l_graph, peaks, path);
         con_comp++;
-        for (auto i : path) top[i - 1] = con_comp;
-        path.erase
+        for (auto i : path) {
+            top[i - 1] = con_comp;
+        }
+        path.clear();
+    }
+    cout << con_comp << endl;
+    for (auto i: top) {
+        cout << i << " ";
     }
 }
