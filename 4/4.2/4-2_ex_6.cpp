46c0ccf06f30464083dd6fe8d826249857d098e2
Auto Committer <auto@commit>
2025-09-24 00:10:03 +0300

    Автоматический коммит 2025-09-24 00:10:02

--- 4/4.2/4-2_ex_6.cpp
+++ 4/4.2/4-2_ex_6.cpp
@@ -3,13 +3,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void comp_dfs(int v, vector <bool> &vis, vector <vector <int>> &al, int &cnt, set <int> &p) {
+void comp_dfs(int v, vector <bool> &vis, vector <vector <int>> &al, int &vc, int &ec) {
     vis[v] = true;
-    p.insert(v);
-    cnt++;
+    vc++;
+    ec += static_cast<int>(al[v].size());
     for (auto u : al[v]) {
         if (vis[u]) continue;
-        comp_dfs(u, vis, al, cnt, p);
+        comp_dfs(u, vis, al, vc, ec);
     }
 }
 
@@ -19,27 +19,23 @@ int main() {
     cout.tie(nullptr);
 
     int n, m; cin >> n >> m;
-    vector <vector <pair<int, vector <int>>>> adj_l(n + 1);
+    vector <vector <int>> adj_l(n + 1);
     for (int i = 1; i <= m; i ++) {
         int u,v; cin >> u >> v;
-        adj_l[u].push_back(v).second.push_back(i);
+        adj_l[u].push_back(v);
         adj_l[v].push_back(u);
     }
     vector <bool> visited(n + 1);
-    vector <int> v_comp(n + 1);
-    set <int> path;
-    int vert = 0, st = 0, con_com = 0;
-    while (vert != n) {
-        for (int i = 1; i <= n; i ++) {
-            if (!visited[i]) {st = i; break;}
+    bool cycle_found = false;
+    for (int i = 1; i <= n; i ++) {
+        if (!visited[i]) {
+            int vertex_cnt = 0, edge_cnt = 0;
+            comp_dfs(i, visited, adj_l, vertex_cnt, edge_cnt);
+            edge_cnt /= 2;
+
+            if (edge_cnt >= vertex_cnt) { cycle_found = true; break;}
         }
-        comp_dfs(st, visited, adj_l, vert, path);
-        con_com++;
-        for (auto u: path) {
-            v_comp[u] = con_com;
-        }
-        path.clear();
     }
 
-
+    cout << "NO";
 }
