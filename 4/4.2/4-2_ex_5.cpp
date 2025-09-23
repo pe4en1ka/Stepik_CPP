edd8456b97d4529ba881652966c26d106b45ee8f
Auto Committer <auto@commit>
2025-09-23 21:10:03 +0300

    Автоматический коммит 2025-09-23 21:10:03

--- 4/4.2/4-2_ex_5.cpp
+++ 4/4.2/4-2_ex_5.cpp
@@ -4,7 +4,15 @@
 using namespace std;
 
 int main() {
-  ios::sync_with_stdio(false);
-  cin.tie(nullptr);
-  cout.tie(nullptr);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int n, m; cin >> n >> m;
+    vector <pair<int, int>> edges(m + 1);
+    for (int i = 1; i <= m; i++) {
+        cin >> edges[i].first >> edges[i].second;
+    }
+    int q; cin >> q;
+     
 }
