fe41a949d78b3fe8b09233316c55aa56a40225bd
Auto Committer <auto@commit>
2025-09-25 22:10:03 +0300

    Автоматический коммит 2025-09-25 22:10:03

--- 4/4.2/4-2_ex_7.cpp
+++ 4/4.2/4-2_ex_7.cpp
@@ -9,4 +9,11 @@ int main() {
     cout.tie(nullptr);
 
     int n, m; cin >> n >> m;
+
+    vector <vector <int>> adj_l(n + 1);
+    for (int i = 1; i <= n; i++) {
+        int u, v; cin >> u >> v;
+        adj_l[u].push_back(v);
+    }
+    vector <char> color(n + 1, 'w');
 }
