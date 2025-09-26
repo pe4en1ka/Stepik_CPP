eaa9b77add24bba6b80071e0b4ea29ac5737566e
Auto Committer <auto@commit>
2025-09-26 23:10:03 +0300

    Автоматический коммит 2025-09-26 23:10:03

--- 4/4.2/4-2_ex_8.cpp
+++ 4/4.2/4-2_ex_8.cpp
@@ -3,13 +3,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void dfs(const int v, const int a, vector <vector <int>> const &al, vector <int> &vis) {
+bool dfs(const int v, const int a, vector <vector <int>> const &al, vector <bool> &vis, vector <int> &cy, vector <int> &pa) {
     vis[v] = true;
     for (auto u : al[v]) {
-        if (u == a) continue;
-        dfs(u, v, al, vis);
+        if (u == a) {
+            continue;
+        }
+        if (!vis[u]){
+            pa[u] = v;
+            if (dfs(u, v, al, vis, cy, pa)) {
+                return true;
+            }
+        }
+        else {
+            cy.push_back(u);
+            for (int cur = v; cur != u; cur = pa[cur]) {
+                cy.push_back(cur);
+            }
+            return true;
+        }
     }
-    
+    return false;
 }
 
 int main() {
@@ -28,12 +42,19 @@ int main() {
         sort(adj_l[i].begin(), adj_l[i].end());
     }
 
-    vector <int> tin(n + 1), tout(n + 1), cycle;
+    vector <int> parent(n + 1),  cycle;
     vector <bool> visited(n + 1);
-    int timer = 0;
     for (int i = 1; i <= n; i++) {
         if (!visited[i]) {
-            dfs()
+            if (dfs(i, -1, adj_l, visited, cycle, parent)) break;
         }
+    }
+    if (cycle.empty()) {
+        cout << "NO" << endl;
+    }
+    else {
+        cout << "YES" << endl;
+        cout << cycle.size() << endl;
+        for (auto x : cycle) cout << x << " ";
     }
 }
