9a184cd2879e407ec8ced98cee359f62a7e07f27
Auto Committer <auto@commit>
2025-09-26 20:10:03 +0300

    Автоматический коммит 2025-09-26 20:10:03

--- 4/4.2/4-2_ex_7.cpp
+++ 4/4.2/4-2_ex_7.cpp
@@ -3,10 +3,16 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool dfs(int v, vector <vector <int>> &al, vector <int> &c, vector <int> &pf, vector <int> &cy) {
+bool dfs(int v, vector <vector <int>> &al, vector <int> &c, vector <int> &pa, vector <int> &cy) {
     c[v] = 1;
     for (auto u : al[v]) {
-        
+        if (c[u] == 0) {
+            pa[u] = v;
+            if (dfs(u, al, c, pa, cy)) return true;
+        }
+        else if (c[u] == 1) {
+            cy.push_back(v);
+        }
     }
 }
 
@@ -26,7 +32,9 @@ int main() {
     vector <int> color(n + 1), parent(n + 1), cycle;
     for (int i = 1; i <= n; i++) {
         if (color[i] == 0) {
-            if ()
+            if (dfs(i, adj_l, color, parent, cycle)) {
+                break;
+            }
         }
     }
 }
