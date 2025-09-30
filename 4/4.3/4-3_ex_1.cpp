961c414e824316a41e79bed7c747a26efc0f59a8
Auto Committer <auto@commit>
2025-09-30 21:10:03 +0300

    Автоматический коммит 2025-09-30 21:10:03

--- 4/4.3/4-3_ex_1.cpp
+++ 4/4.3/4-3_ex_1.cpp
@@ -9,6 +9,27 @@ int main() {
     cout.tie(nullptr);
 
     int n, m; cin >> n >> m;
-    vector <vector <int>> adj_k(n + 1);
-    
+    vector <vector <int>> adj_l(n + 1);
+    queue <int> q;
+    vector <int> waves;
+    vector <bool> used (n + 1, false);
+    for (int i = 1; i <= m; i++) {
+        int u, v; cin >> u >> v;
+        adj_l[u].push_back(v);
+        adj_l[v].push_back(u);
+    }
+    int s; cin >> s;
+    q.push(s);
+    used[s] = true;
+
+    while (!q.empty()) {
+        int v = q.front();
+        q.pop();
+        for (auto u : adj_l[v]) {
+            if (used[u]) continue;
+            used[u] = true;
+            q.push(u);
+            waves.push_back(u);
+        }
+    }
 }
