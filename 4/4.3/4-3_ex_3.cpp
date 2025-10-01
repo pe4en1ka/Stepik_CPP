2dfa9eee1912d3a762994f0491b8863a60d5ffd4
Auto Committer <auto@commit>
2025-10-01 23:10:03 +0300

    Автоматический коммит 2025-10-01 23:10:03

---
+++ 4/4.3/4-3_ex_3.cpp
@@ -0,0 +1,15 @@
+//          For stepik
+//          Made by coder228
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int n, m; cin >> n >> m;
+    vector <vector <pair <int, int>>> adj_l(n + 1, vector <pair <int, int>>());
+    int p, q; cin >> p >> q;
+    int x1, y1, x2, y2; cin >> x1 >> y1 >> x2 >> y2;
+}
