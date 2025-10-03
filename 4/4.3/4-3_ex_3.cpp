7b51ea2c9c22390368e3746df69a580f282e6b86
Auto Committer <auto@commit>
2025-10-04 01:10:04 +0300

    Автоматический коммит 2025-10-04 01:10:04

--- 4/4.3/4-3_ex_3.cpp
+++ 4/4.3/4-3_ex_3.cpp
@@ -9,7 +9,24 @@ int main() {
     cout.tie(nullptr);
 
     int n, m; cin >> n >> m;
-    vector <vector <pair <int, int>>> adj_l(n + 1, vector <pair <int, int>>());
+    vector <vector <int>> dist(n + 1, vector <int>(m + 1, -1));
     int p, q; cin >> p >> q;
     int x1, y1, x2, y2; cin >> x1 >> y1 >> x2 >> y2;
+    vector <pair<int, int>> step = {
+        {p,q}, {p, -q}, {-p, q}, {-p, -q},
+        {q, p}, {q, -p}, {-q, p}, {-q, -p}
+    };
+    queue <pair<int, int>> qv;
+    auto check = [&](int x, int y) {return 1 <= x && x <= n && 1 <= y && y <= m; };
+
+    dist[x1][y1] = 0;
+    qv.push({x1, y1});
+    while (!qv.empty()) {
+        pair <int, int> cord = qv.front();
+        int x = cord.first, y = cord.second;
+        qv.pop();
+        for (auto ch : step) {
+            int nx = ch.first + x, ny = ch.second + y;
+        }
+    }
 }
