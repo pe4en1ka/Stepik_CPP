4cd71d7236d48d0d0ae6900f80c99d9430f31150
Auto Committer <auto@commit>
2025-10-07 00:10:03 +0300

    Автоматический коммит 2025-10-07 00:10:03

---
+++ 4/4.3/4-3_ex_4.cpp
@@ -0,0 +1,13 @@
+//          For stepik
+//          Made by coder228
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int n, m; cin >> n >> m;
+
+}
