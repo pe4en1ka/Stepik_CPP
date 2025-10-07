ee9c707963557568e9e2f0c988d71ff5142cc314
Auto Committer <auto@commit>
2025-10-08 00:10:03 +0300

    Автоматический коммит 2025-10-08 00:10:03

--- 4/4.3/4-3_ex_4.cpp
+++ 4/4.3/4-3_ex_4.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 vector <pair<int, int>> ducks;
 pair <int, int> stock;
-ve
+vector <char> dir = {'u', 'r', 'd', 'l'};
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
@@ -15,16 +15,24 @@ int main() {
     vector <vector <int>> walls(2 * n + 1, vector <int>(2 * m + 1));
     vector <vector <int>> dist(2 * n + 1, vector <int>(2 * m + 1, -1));
     vector <vector <pair<int, int>>> par(2 * n + 1, vector <pair <int, int>> (2 * m + 1, {0, 0}));
-    for (int i = 1; i <= 2 * n; i++) {
-        for (int j = 1; j <= 2 * m; j++) {
-            char tmp; cin >> tmp;
-            if (tmp == 'D') ducks.push_back({i, j});
-            if (tmp == 'S') stock = {i, j};
-            if (tmp == '|' or tmp == '-') walls[i][j] = 1;
-            if (tmp == ' ') walls[i][j] = 0;
-            else walls[i][j] = -1;
+    for (int i = 1; i <= 2 * n + 1; i++) {
+        string s;
+        getline(cin, s);
+        getline(cin, s);
+        for (auto j = 0; j < s.length(); j++) {
+            char c = s[j];
+            if (c == 'D') ducks.push_back({i, j});
+            if (c == 'S') stock = {i, j};
+            if (c == '|') walls[i][j] = 1;
+            if ( c == '-') walls[i][j] = 2;
+            if (c == ' ') walls[i][j] = 0;
+            if (c == '+') walls[i][j] = -1;
         }
     }
+    auto next_ceil = [&](int x, int y, char drctn) {
+
+    };
+
     auto dfs = [&](const pair <int, int> &ceil) {
         int x1 = ceil.first, y1 = ceil.second;
         queue <pair <int, int>> q;
@@ -34,6 +42,9 @@ int main() {
         while (!q.empty()) {
             pair <int, int> c = q.front();
             int x = c.first, y = c.second;
+            for (char d : dir) {
+
+            }
         }
     };
 }
