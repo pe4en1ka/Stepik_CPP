ec8b3045d6770899f16a772cefb6b8fa5f816753
Auto Committer <auto@commit>
2025-10-08 01:10:03 +0300

    Автоматический коммит 2025-10-08 01:10:03

--- 4/4.3/4-3_ex_4.cpp
+++ 4/4.3/4-3_ex_4.cpp
@@ -12,39 +12,68 @@ int main() {
     cout.tie(nullptr);
 
     int n, m; cin >> n >> m;
-    vector <vector <int>> walls(2 * n + 1, vector <int>(2 * m + 1));
-    vector <vector <int>> dist(2 * n + 1, vector <int>(2 * m + 1, -1));
-    vector <vector <pair<int, int>>> par(2 * n + 1, vector <pair <int, int>> (2 * m + 1, {0, 0}));
+    vector <vector <int>> walls(2 * n + 1 + 1, vector <int>(2 * m + 1));
+    vector <vector <int>> dist(2 * n + 1 + 1, vector <int>(2 * m + 1, -1));
+    vector <vector <pair<int, int>>> par(2 * n + 1 + 1, vector <pair <int, int>> (2 * m + 1, {0, 0}));
+    string s;
+    getline(cin, s);
     for (int i = 1; i <= 2 * n + 1; i++) {
-        string s;
-        getline(cin, s);
         getline(cin, s);
         for (auto j = 0; j < s.length(); j++) {
             char c = s[j];
-            if (c == 'D') ducks.push_back({i, j});
-            if (c == 'S') stock = {i, j};
-            if (c == '|') walls[i][j] = 1;
-            if ( c == '-') walls[i][j] = 2;
-            if (c == ' ') walls[i][j] = 0;
-            if (c == '+') walls[i][j] = -1;
+            if (c == 'D') {ducks.emplace_back(i, j + 1); walls[i][j + 1] = 0;}
+            if (c == 'S') {stock = {i, j + 1}; walls[i][j + 1] = 0;}
+            if (c == '|') walls[i][j + 1] = 1;
+            if (c == '-') walls[i][j + 1] = 2;
+            if (c == '+') walls[i][j + 1] = 3;
+            if (c == ' ') walls[i][j + 1] = 0;
         }
+        cout << s << endl;
     }
-    auto next_ceil = [&](int x, int y, char drctn) {
-
+    int x2 = stock.first, y2 = stock.second;
+    auto next_ceil = [&](const int &x, const int &y, const char &drctn) {
+        if (drctn == 'u') {
+            for (int i = x; i >= 1; i--) {
+                if (walls[i][y] == 2 || walls[i][y] == 3) {pair <int, int> p = {i + 1, y}; return p;}
+            }
+        }
+        else if (drctn == 'r') {
+            for (int j = y; j <= 2 * m + 1; j++) {
+                if (walls[x][j] == 1 || walls[x][j] == 3) {pair <int, int> p = {x, j - 1}; return p;}
+            }
+        }
+        else if (drctn == 'd') {
+            for (int i = x; i <= 2 * n + 1; i++) {
+                if (walls[i][y] == 2 || walls[i][y] == 3) {pair <int, int> p = {i - 1, y}; return p;}
+            }
+        }
+        else {
+            for (int j = y; j >= 1; j--) {
+                if (walls[x][j] == 1 || walls[x][j] == 3) {pair <int, int> p = {x, j + 1}; return p;}
+            }
+        }
     };
-
     auto dfs = [&](const pair <int, int> &ceil) {
         int x1 = ceil.first, y1 = ceil.second;
         queue <pair <int, int>> q;
         dist[x1][y1] = 0;
         par[x1][y1] = {0, 0};
-        q.push({x1, y1});
+        q.emplace(x1, y1);
         while (!q.empty()) {
             pair <int, int> c = q.front();
             int x = c.first, y = c.second;
+            q.pop();
             for (char d : dir) {
-
+                pair <int, int> nc = next_ceil(x, y, d);
+                int nx = nc.first, ny = nc.second;
+                dist[nx][ny] = dist[x][y] + 1;
+                par[nx][ny] = {x, y};
+                q.emplace(nx, ny);
             }
         }
+        if (dist[x2][y2] == -1) walls[x2][y2] = 0;
     };
+    for (auto duck : ducks) {
+        dfs(duck);
+    }
 }
